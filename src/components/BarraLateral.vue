85e4a0c404c63cc4bf6e3c2be629ad8d1db257af
Eduardo Teixeira <dunirvana@gmail.com>
2022-06-23 17:12:46 -0300

    Adicionado menu de navegação e view inicial para projetos.

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -4,9 +4,27 @@
       <img src="../assets/logo.png" alt="">
       <span>myTracker</span>
     </h1>
-    <button class="button" @click="alterarTema">
-      {{ textoDoBotao }}
-    </button>
+    <div class="has-text-centered">
+      <button class="button" @click="alterarTema">
+        {{ textoDoBotao }}
+      </button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            Tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            Projetos
+          </router-link>
+        </li>        
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -58,5 +76,17 @@ span {
     padding: 2.5rem;
     height: auto;
   }
+}
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #FAF0CA;
+}
+.link.router-link-active {
+  color: #FAF0CA;
 }
 </style>
