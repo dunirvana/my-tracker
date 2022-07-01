58cdfe988f054643de0a1745db2e35b79c90e5d3
Eduardo Teixeira <dunirvana@gmail.com>
2022-07-01 11:49:26 -0300

    Projetos direto da store

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -28,25 +28,26 @@
 </template>
 
 <script lang="ts">
-import IProjeto from '@/interfaces/IProjeto';
-import { defineComponent } from 'vue';
+import { useStore } from '@/store';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Projetos',
+  setup() {
+    const store = useStore();
+    return {
+      store,
+      projetos: computed(() => store.state.projetos)
+    }
+  },
   data() {
     return {
-      nomeDoProjeto: '',
-      projetos: [] as IProjeto[]
+      nomeDoProjeto: ''
     }
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString()
-      };
-
-      this.projetos.push(projeto);
+      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
       this.nomeDoProjeto = '';
     }
   }
