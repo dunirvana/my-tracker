9439f42f5aee09012420dade0ea84078da0e28ff
Eduardo Teixeira <dunirvana@gmail.com>
2022-07-01 14:35:31 -0300

    View de projeto separada em lista e formulario, inclusão da edição de projetos.

---
+++ src/views/Projetos/Formulario.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
+        <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto" />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { useStore } from '@/store';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Formulario',
+  setup() {
+    const store = useStore();
+    return {
+      store
+    }
+  },
+  props: {
+    id: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      nomeDoProjeto: ''
+    }
+  },
+  mounted() {    
+    if (this.id) {
+      console.log('mounted');
+      const projeto = this.store.state.projetos.find(pr => pr.id == this.id);
+      this.nomeDoProjeto = projeto === undefined ? '' : projeto.nome;
+    }
+  },
+  methods: {
+    salvar() {
+      if(this.id) {
+        this.store.commit('ALTERA_PROJETO', {
+          id: this.id,
+          nome: this.nomeDoProjeto
+        });
+      }
+      else {
+        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
+      } 
+
+      this.nomeDoProjeto = '';
+      this.$router.push('/projetos');
+    }
+  }
+});
+
+</script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;;
+}
+</style>
+
